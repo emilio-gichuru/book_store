14ced0fc9a01e8503ff8e13242d7550a7e6fdcfc
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-10-26 21:23:28 +0300

    add: added the new pages

--- lib/utilities/app_constants.dart
+++ lib/utilities/app_constants.dart
@@ -1,3 +1,5 @@
+
+
 const bookCategories = [
   {
     "name": "Fiction",
---
+++ lib/utilities/app_images.dart
@@ -0,0 +1,2 @@
+const defaultImage =
+    "https://firebasestorage.googleapis.com/v0/b/apt-rite-346310.appspot.com/o/book%2Fbrave_new_world.jpeg?alt=media&token=d51ee31a-6908-4b05-acba-1475dc58651b";
